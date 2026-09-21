1e3738b334115a112cad7e7e9e7142bf33f130d4
silok2239 <silok2239@gmail.com>
2026-09-21 22:09:27 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='procurement-quiz-v2';
+const ASSETS=['./','./index.html','./questions.json','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
